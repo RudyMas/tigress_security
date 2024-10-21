d3fabe4ed344418913cbb62ff08a4a2c21f15341
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-21 13:18:45 +0200

    Added checkAccess & checkRegerer

--- src/Security.php
+++ src/Security.php
@@ -10,12 +10,14 @@ use Random\RandomException;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 1.0.0
+ * @version 1.1.0
  * @lastmodified 2024-10-10
  * @package Tigress\Security
  */
 class Security
 {
+    private array $sites = ['localhost'];
+
     /**
      * Get the version of the class
      *
@@ -23,7 +25,54 @@ class Security
      */
     public static function version(): string
     {
-        return '1.0.0';
+        return '1.1.0';
+    }
+
+    /**
+     * Check if the request comes from the website
+     *
+     * @return void
+     */
+    public static function checkAccess(): void
+    {
+        if (isset($_SERVER['HTTP_REFERER'])) {
+            $referer = parse_url($_SERVER['HTTP_REFERER']);
+            if (!in_array($referer['host'], self::$sites)) {
+                header('HTTP/1.0 403 Forbidden');
+                exit;
+            }
+        } else {
+            header('HTTP/1.0 403 Forbidden');
+            exit;
+        }
+    }
+
+    /**
+     * Check if the request comes from a certain path
+     *
+     * @param array $referencePaths
+     * @return void
+     */
+    public static function checkReferer(array $referencePaths): void
+    {
+        $referenceOkay = false;
+        foreach ($referencePaths as $referencePath) {
+            if (isset($_SERVER['HTTP_REFERER'])) {
+                $referer = parse_url($_SERVER['HTTP_REFERER']);
+                $referees = explode('/', $referer['path']);
+                $paths = explode('/', $referencePath);
+                for ($i = 0; $i < count($paths) && $i < count($referees); $i++) {
+                    if ($paths[$i] === $referees[$i] || $paths[$i] === '*') {
+                        $referenceOkay = true;
+                    }
+                }
+            }
+        }
+
+        if (!$referenceOkay) {
+            header('HTTP/1.0 403 Forbidden');
+            exit;
+        }
     }
 
     /**
@@ -60,5 +109,16 @@ class Security
     public function verifyHash(string $password, string $salt, string $hash): bool
     {
         return hash_equals($hash, $this->createHash($password, $salt));
+    }
+
+    /**
+     * Set the sites
+     *
+     * @param array $sites
+     * @return void
+     */
+    public function setSites(array $sites): void
+    {
+        $this->sites = $sites;
     }
 }
