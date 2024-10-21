bcbb10fec26cf02400d0c3575d850b320ee3c5c0
RudyMasGoNext <rudy.mas@go-next.be>
2024-10-21 14:19:52 +0200

    Added code to check matching paths

--- src/Security.php
+++ src/Security.php
@@ -10,7 +10,7 @@ use Random\RandomException;
  * @author Rudy Mas <rudy.mas@rudymas.be>
  * @copyright 2024, rudymas.be. (http://www.rudymas.be/)
  * @license https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version 1.1.1
+ * @version 1.1.2
  * @lastmodified 2024-10-21
  * @package Tigress\Security
  */
@@ -25,7 +25,7 @@ class Security
      */
     public static function version(): string
     {
-        return '1.1.1';
+        return '1.1.2';
     }
 
     public function __construct()
@@ -64,24 +64,70 @@ class Security
      */
     public static function checkReferer(array $referencePaths): void
     {
-        $referenceOkay = false;
-        foreach ($referencePaths as $referencePath) {
-            if (isset($_SERVER['HTTP_REFERER'])) {
-                $referer = parse_url($_SERVER['HTTP_REFERER']);
-                $referees = explode('/', $referer['path']);
-                $paths = explode('/', $referencePath);
-                for ($i = 0; $i < count($paths) && $i < count($referees); $i++) {
-                    if ($paths[$i] === $referees[$i] || $paths[$i] === '*') {
-                        $referenceOkay = true;
-                    }
-                }
-            }
-        }
+        $referenceOkay = self::pathMatches($_SERVER['HTTP_REFERER'], $referencePaths);
 
         if (!$referenceOkay) {
             header('HTTP/1.0 403 Forbidden');
             exit;
         }
+    }
+
+    /**
+     * Check if the request comes from a certain path
+     *
+     * @param $urlPath
+     * @param $referencePaths
+     * @return bool
+     */
+    private static function pathMatches($urlPath, $referencePaths): bool
+    {
+        $return = false;
+        // Break down the URL path into an array
+        $referer = parse_url($urlPath);
+        $referees = explode('/', trim($referer['path'], '/')); // Trimming leading/trailing slashes
+
+        // Loop through each possible reference path
+        foreach ($referencePaths as $referencePath) {
+            // Break down the reference path into an array
+            $paths = explode('/', trim($referencePath, '/'));
+
+            // Check if the paths match
+            if (self::pathsMatch($referees, $paths)) {
+                return true;
+            }
+        }
+
+        return false; // If no path matches
+    }
+
+    /**
+     * Check if the URL path matches the reference path
+     *
+     * @param $referees
+     * @param $paths
+     * @return bool
+     */
+    private static function pathsMatch($referees, $paths): bool
+    {
+        // If the reference path is shorter than the URL path and doesn't contain a wildcard, it's not a match
+        if (count($paths) < count($referees)) {
+            return false;
+        }
+
+        // Loop through each segment of the reference path
+        foreach ($paths as $index => $segment) {
+            // If wildcard (*), this segment matches anything (or nothing)
+            if ($segment === '*') {
+                continue;
+            }
+
+            // Check if the URL path has a corresponding segment to compare
+            if (!isset($referees[$index]) || $referees[$index] !== $segment) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
